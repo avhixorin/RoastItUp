6ed339d78099b35a17c43c47d5cd6f30b0fbb6a0
avhixorin <avhixorin@gmail.com>
2025-05-25 23:35:26 +0530

    fix: remove unused EyeClosed import from register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link";
-import { Eye, EyeClosed, EyeOff, FlameIcon } from "lucide-react";
+import { Eye, EyeOff, FlameIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
